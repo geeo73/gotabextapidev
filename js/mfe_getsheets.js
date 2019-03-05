769756522bd9774bd4b3d3ed37fe707e93bef673
geeo73 <32762376+geeo73@users.noreply.github.com>
2019-03-05 16:28:21 +0000

    Updates

--- js/mfe_getsheets.js
+++ js/mfe_getsheets.js
@@ -4,7 +4,31 @@
 (function () {
   // Use the jQuery document ready signal to know when everything has been initialized
   $(document).ready(function () {
-    // Add your startup code here
-
+    // Tell Tableau we'd like to initialize our extension
+    tableau.extensions.initializeAsync().then(function () {
+      // Once the extension is initialized, ask the user to choose a sheet
+      showSheetNames();
+    });
   });
+
+    /**
+   * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+   */
+  function showSheetNames () {
+    
+    // Set the dashboard's name in the title
+    const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
+    $('#dashboard_name').text(dashboardName);
+
+    // The first step in choosing a sheet will be asking Tableau what sheets are available
+    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+
+    // Next, we loop through all of these worksheets and pop an alert with it's name
+    worksheets.forEach(function (worksheet) {
+      // For now, just pop up an alert saying that we've selected a sheet
+      alert(`Worksheet ... ${worksheetName}`);
+
+    });
+  }
+
 })();
